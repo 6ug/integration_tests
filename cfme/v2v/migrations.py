7a5a6b8c1cf0d4c0f155e57bc3b0b1bc572c066a
Yadnyawalkya <ytale@redhat.com>
2018-05-09 20:56:06 +0530

    working tables, added switch, working on switch handling and data_path imports may9

--- cfme/v2v/migrations.py
+++ cfme/v2v/migrations.py
@@ -2,55 +2,91 @@ import attr
 
 from navmazing import NavigateToAttribute, NavigateToSibling
 from cfme.base.login import BaseLoggedInPage
-
-from cfme.utils.pretty import Pretty
-from widgetastic.widget import View
-from widgetastic_patternfly import Text, BootstrapSelect, Input
-from widgetastic_manageiq import RadioGroup, CheckboxSelect, Button
+from widgetastic.widget import View, Table, ConditionalSwitchableView
+from widgetastic_manageiq import RadioGroup
+from widgetastic_patternfly import Text, TextInput, Button, BootstrapSelect
 from cfme.modeling.base import BaseCollection, BaseEntity
-from cfme.utils.appliance.implementations.ui import navigator, CFMENavigateStep
+from cfme.utils.appliance.implementations.ui import (navigator, CFMENavigateStep,
+    can_skip_badness_test)
 
 
-# I have to add four classes with working widgets
+# Views
 
-# class MigrationPlanWizardView(View):
-#     infra_mapping = BootstrapSelect('infrastructure_mapping')
-#     name = Input(name='name')
-#     description = Input(name='description')
-#     vms_imports = RadioGroup('//input[@name="vm_choice_radio"]') # not tested
-#
-#     next_btn = Button(title='Next')
-#     cancel_btn = Button(title='Cancel')
-#     back_btn = Button(title='Back')
-#
-
-
-# views
 
 class MigrationDashboardView(BaseLoggedInPage):
     create_infrastructure_mapping = Text(locator='(//a|//button)'
         '[text()="Create Infrastructure Mapping"]')
     create_migration_plan = Text(locator='(//a|//button)[text()="Create Migration Plan"]')
 
-    # we have to remove below, I just added for testing
-
-    infra_mapping = BootstrapSelect('infrastructure_mapping')
-    name = Input(name='name')
-    description = Input(name='description')
-    vms_imports = RadioGroup('//input[@name="vm_choice_radio"]') # not working
-
-    next_btn = Button(title='Next')
-    cancel_btn = Button(title='Cancel')
-    back_btn = Button(title='Back')
-
-
     @property
     def is_displayed(self):
         return self.navigation.currently_selected == ['Compute', 'Migration']
 
 
-# Collections Entities
+class AddInfrastructureMappingView(View):
+    title = Text(locator='.//h4[contains(@class,"modal-title")]')
+    name = TextInput(name='name')
+    description = TextInput(name='description')
+    back_btn = Button('Back')
+    next_btn = Button('Next')
+    cancel_btn = Button('Cancel')
 
+
+    @property
+    def is_displayed(self):
+        return (self.title.text == 'Infrastructure Mapping Wizard')
+
+
+class AddMigrationPlanView(View):
+    title = Text(locator='.//h4[contains(@class,"modal-title")]') # tested
+    infra_map = BootstrapSelect('infrastructure_mapping') # tested
+    name = TextInput(name='name') # tested
+    description = TextInput(name='description') # tested
+    back_btn = Button('Back') # tested
+    next_btn = Button('Next') # tested
+    cancel_btn = Button('Cancel') # tested
+    msg = './/div[6]/div[2]/div/div/div/div[2]/section/div/div/div/h3/text()'  # tested
+
+
+    # adding temp xpath, issue created at https://github.com/priley86/miq_v2v_ui_plugin/issues/305
+    select_vm = RadioGroup('.//div[6]/div[2]/div/div/div/div[2]/section/div/div/form/div[4]/div')
+    switch1 = ConditionalSwitchableView(reference="select_vm.interval")
+    # con1 and con2 need to placed properly into conditional switch, not optimised
+    con1 = "Import a CSV file with a list of VMs to be migrated"
+    con2 = "Choose from a list of VMs discovered in the selected infrastructure mapping"
+
+    @switch1.register(con1)
+    class ImportCSV:
+        pass
+        #Todo: importing via data_path
+
+    @switch1.register(con2, default=True)
+    class DiscoverVM:
+        table = Table('.//*[contains(@class, "container-fluid")]/table')
+
+
+    run_migration = RadioGroup('.//div[6]/div[2]/div/div/div/div[2]/section/div/div/form/div/div')
+    switch2 = ConditionalSwitchableView(reference="run_migration.interval")
+    # con3 and con4 need to placed properly into conditional switch, not optimised
+    con3 = 'Save migration plan to run later'
+    con4 = 'Start migration immediately'
+
+    @switch2.register(con3, default=True)
+    class SaveMigration:
+        pass
+        #Todo: SaveMigration
+
+    @switch2.register(con4)
+    class DiscoverVM:
+        pass
+        #Todo: SaveMigration
+
+    @property
+    def is_displayed(self):
+        return (self.title.text == 'Migration Plan Wizard')
+
+
+# Collections Entities
 
 @attr.s
 class InfrastructureMapping(BaseEntity):
@@ -78,6 +114,8 @@ class MigrationPlanCollection(BaseCollection):
     ENTITY = MigrationPlan
 
 
+# Navigations
+
 @navigator.register(InfrastructureMappingCollection, 'All')
 @navigator.register(MigrationPlanCollection, 'All')
 class All(CFMENavigateStep):
@@ -93,18 +131,28 @@ class All(CFMENavigateStep):
 
 
 @navigator.register(InfrastructureMappingCollection, 'Add')
-class InfrastructureMappingAdd(CFMENavigateStep):
-    VIEW = MigrationDashboardView
+class AddInfrastructureMapping(CFMENavigateStep):
+    VIEW = AddInfrastructureMappingView
     prerequisite = NavigateToSibling('All')
 
     def step(self):
         self.prerequisite_view.create_infrastructure_mapping.click()
 
-# working, just use like, view = navigate_to(appliance.collection.migration_plan, 'Add')
+    @can_skip_badness_test  # because it loops over and over as it cannot handle the modal
+    def resetter(self):
+        # Reset the view
+        self.view.browser.refresh()
+
+
 @navigator.register(MigrationPlanCollection, 'Add')
-class MigrationPlanAdd(CFMENavigateStep):
-    VIEW = MigrationDashboardView
+class AddMigrationPlan(CFMENavigateStep):
+    VIEW = AddMigrationPlanView
     prerequisite = NavigateToSibling('All')
 
     def step(self):
         self.prerequisite_view.create_migration_plan.click()
+
+    @can_skip_badness_test  # because it loops over and over as it cannot handle the modal
+    def resetter(self):
+        # Reset the view
+        self.view.browser.refresh()
